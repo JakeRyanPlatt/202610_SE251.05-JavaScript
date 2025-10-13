3e574f444abe2481af84cbc6d5dc03e31da1f8c0
JakeRyanPlatt <jakeplatt176290@gmail.com>
2025-10-13 19:13:55 -0400

    Player 2 class added for week 2

---
+++ 202610_SE251.05-JavaScript/Week2/WK2/player.js
@@ -0,0 +1,10 @@
+class player
+    {
+        constructor(this.name,this.score,this.highScore,this.pad)
+        {
+            this.name;
+            this.score;
+            this.highScore;
+            this.pad;
+        }
+    } 
