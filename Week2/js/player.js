7658b202504d68a169fd96da4958004475e19f8a
JakeRyanPlatt <jakeplatt176290@gmail.com>
2025-11-03 18:24:39 -0500

    fixed file path

--- Week2/js/player.js
+++ Week2/js/player.js
@@ -1,10 +1,10 @@
 class Player {
-    constructor(name = "Player") {
+    constructor(name = "", pad = null) {
         this.name = name;
         this.score = 0;
         this.highScore = 0;
         // pad will be a Box instance representing the player's paddle
         // Create a default Box so the Player always has a pad available
-        this.pad = new Box();
+        this.pad = pad;
     }
 }
